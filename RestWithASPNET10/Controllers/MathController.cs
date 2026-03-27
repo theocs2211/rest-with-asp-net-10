7457fa9df7227f96226a65f7a4c2f74de60986a2
Theo <theocs2211@gmail.com>
2026-03-27 17:30:22 -0300

    Endpoint get de soma com parametros via body adicionado

---
+++ RestWithASPNET10/Controllers/MathController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET10.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MathController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum);
+            }
+            return BadRequest("Invalid Input!");
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            decimal decimalValue;
+            bool isNumber = decimal.TryParse(strNumber, 
+                System.Globalization.NumberStyles.Any, 
+                System.Globalization.NumberFormatInfo.InvariantInfo, 
+                out decimalValue);
+            return isNumber;
+        }
+    }
+}
